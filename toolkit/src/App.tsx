9fb7fe08beed6f6e967244b851f0b0cdd1455085
alimombeni <ali.mombenni@gmail.com>
2024-09-08 19:00:31 +0330

    [ADD:render components in App]

--- toolkit/src/App.tsx
+++ toolkit/src/App.tsx
@@ -1,12 +1,20 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-
-
+import Product from "./component/Product.tsx";
+import ProductList from "./component/ProductList.tsx";
+import { productData } from './data/items.ts'
 function App () {
     return (
         <Provider store={store}>
-            <div>App</div>
+         <ProductList>
+             {productData.map((item)=> (
+                 <div key={item.id}>
+                     <Product {...item} />
+                 </div>
+             ))}
+         </ProductList>
+
         </Provider>
 
     )
